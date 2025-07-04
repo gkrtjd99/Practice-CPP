a01499dfe16d60cbd6c9a562f1e2a3f8f0a555d7
hakslee <gkrtjd999@gmail.com>
2025-07-04 19:05:08 +0900

    Chapter02 update

---
+++ Chapter03/first_name.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    string  first_name = "???";
+    int     age = -1;
+
+    cin >> first_name >> age;
+    cout << first_name << age << "\n";
+}
